85a1c2e75d7aac45c33e818bbe7a0aeaeb6e4688
sukso96100 <sukso96100@gmail.com>
2014-01-26 16:39:05 +0900

    renewing the site...

---
+++ javascript/index.js
@@ -0,0 +1,6 @@
+ $(document).ready(function() {
+                        $.fn.fullpage({
+                                slidesColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+                                anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage']
+                        });
+                });

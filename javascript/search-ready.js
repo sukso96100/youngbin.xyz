cc032ae9e711ea0b0ff35cb652be8d9011e50f60
sukso96100 <sukso96100@gmail.com>
2014-05-18 19:40:11 +0900

    update

--- javascript/search-ready.js
+++ javascript/search-ready.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
-    $('.search-field').simpleJekyllSearch();
-});
+var searchToggle = document.querySelectorAll(".search-toggle")[0],
+	JSloaded = false;
+searchToggle.onclick = function( e ){
+	var search = document.querySelectorAll(".search")[0],
+		results = document.querySelectorAll(".results")[0];
+
+	search.classList.toggle("show");
+	results.classList.toggle("show");
+
+	results.innerHTML = "";
+
+	setTimeout(function () {
+		search.value = "";
+		search.focus();
+	},100);
+
+	if( !JSloaded ){
+		JSloaded = true;
+		JekyllSearch.init({
+			//fuzzy: true,
+			limit: 10,
+			template: '<a href="{url}" title="{desc}" class="search-result-item">{title}</a>',
+			searchResultsHeader: '<h4>Search results</h4><span class="nano-copy">powered by <a style="display:inline-block;" href="http://christian-fei.com/simple-jekyll-search-jquery-plugin/">Simple Jekyll Search</a></span>'
+		});
+	}
+};
